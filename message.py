8669128d67f8287da91e40503456a1d392c001bc
Le Cong Hau <anhlahau.hl@gmail.com>
2020-09-03 19:20:10 +0700

    Add chart

--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 import pyrebase
 import json
 import os
-
+import datetime
 
 
 
@@ -29,27 +29,58 @@ class Experiment(object):
 		with open(api_path) as file:
 			self.user_api = file.read().strip()
 
+		self.exp_path = "experiments/{}/{}".format(self.user_api, self.name)
 		if clear_old:
 			self.clear()
 
+		db.child(self.exp_path).set({	'start_time': datetime.datetime.now().isoformat(), 
+						'latest_time': datetime.datetime.now().isoformat()})
+
 
 	def clear(self):
-		db.child("experiments").child(self.user_api).child(self.name).remove()
+		db.child(self.exp_path).remove()
 
 
 	def metric(self, met, mss):
-		db.child("experiments").child(self.user_api).child(self.name).child('metric').child(met).push(float(mss))
+		db.child(self.exp_path).child('metric').child(met).push(float(mss))
 
 	def param(self, par, mss):
-		db.child("experiments").child(self.user_api).child(self.name).child('param').child(par).set(json.dumps(mss))
+		db.child(self.exp_path).child('param').child(par).set(json.dumps(mss))
+
+	def log(self, mss):
+		log_time = datetime.datetime.now().isoformat().split('.')[0]
+		db.child(self.exp_path).child('logs').push(log_time + ': ' + mss)
+
+	def command(self, mss):
+		try:
+			db.child(self.exp_path).child('eval').push(str(eval(mss)))
+		except Exception as e:
+			db.child(self.exp_path).child('eval').push(str(e))
 
 	def capture_log(self):
 		pass
 
 
-test = Experiment('Hello')
-test.param('new', dict({'c': 1, 'd': 6}))
+test = Experiment('Exp_name_3')
+test.param('newa', dict({'c': 1, 'd': 6}))
 test.param('newb', dict({'c': 1, 'd': 6}))
 test.param('newc', dict({'c': 1, 'd': 6}))
-test.metric('acc', 10)
-test.metric('acc', 122)
+
+# for i in range(100):
+# 	test.metric('acc', i)
+
+
+# for i in range(100):
+# 	test.metric('loss', (100 - i) / 100)
+
+
+import time
+import random
+i = 0
+while True:
+	test.log('Hello' + str(time.time()))
+	test.metric('acc', random.randint(0, 100))
+	test.metric('loss', random.randint(0, 10))
+	time.sleep(3)
+	print(i, 'cc')
+	i+=1
